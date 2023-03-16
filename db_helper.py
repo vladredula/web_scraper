ec4729aaf48a94530bdb6096dff59297d396cd56
Vladimer Redula <vdredula@gmail.com>
2023-03-16 18:20:50 +0900

    modified db_funtion.py to db_helper.py

---
+++ db_helper.py
@@ -0,0 +1,33 @@
+def create_database(dbname):
+    sql = f'CREATE DATABASE {dbname}'
+
+    return sql
+
+    
+def create_table(tname, fields, pkey):
+    sql = f'CREATE TABLE {tname} ('
+
+    # check if the table already exist
+    if table_exists(tname):
+        return
+
+    for count, (column, attribute) in enumerate(fields.items()):
+        sql += column+' '+attribute.upper()
+
+        if attribute == pkey:
+            sql += ' PRIMARY KEY'
+
+        if count+1 < len(fields):
+            sql += ', '
+    
+    sql += ') '
+
+    return sql
+
+
+def table_exists(tname):
+    sql = f"SELECT EXISTS (SELECT FROM pg_catalog.pg_tables WHERE tablename = '{tname}')"
+    
+    return sql
+    
+
