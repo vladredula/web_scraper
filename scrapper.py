aa4d5b168fdb1089ac356223bb94e283ae886fac
Vladimer Redula <vdredula@gmail.com>
2023-03-17 13:08:05 +0900

    added funtions to extract prices from string and make abbriviations for categories

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 from item import Food, Drink
 
 def getContent(url):
@@ -8,9 +9,39 @@ def getContent(url):
     content = BeautifulSoup(content, "html.parser")
     return content
 
+
 def getStrippedString(str):
     str = str.stripped_strings
     return list(str)
+
+def extract_price(str):
+    str = ''.join(c for c in str if c.isalnum())
+    integers = re.findall(r'\d+', str)
+
+    price = []
+
+    for i in integers:
+        price.append(int(i))
+
+    return price[1]
+
+def make_abbreviation(str):
+    if str == None:
+        return None
+    
+    abbreviation = str[0]
+
+    vowels = 'AEIOU'
+    new_str = ''
+    for s in str[1:]:
+        if s not in vowels and s.isalnum():
+            new_str += s
+
+    for i in range(3):
+        if i < len(new_str):
+            abbreviation += new_str[i]
+
+    return abbreviation.upper()
 
 
 soup = getContent("https://www.tgifridays.co.jp/foods/")
@@ -25,18 +56,20 @@ food_category = dict()
 for a in aList:
     id = a['rel'][0].replace('#','')
     category_name = a.text
-    food_category[id] = category_name
+    food_category[category_name] = id
 
 
-for id in food_category.keys():
+for category_name, id in food_category.items():
     li = soup.find("div", {"id":id})
     liList = li.findAll("li", class_="menu-item")
 
     # getting the list of food under each category
-    sub_cat = ''
+    food_category[category_name] = []
+    sub_cat = None
     for li in liList:
         if len(li['class']) > 1:
             sub_cat = li.text
+            food_category[category_name].append(sub_cat)
             continue
 
         food = getStrippedString(li.a)
@@ -52,31 +85,35 @@ for id in food_category.keys():
             details = details[0].rsplit('。', 1)
 
         detail = details[0].replace(tname, "")
-        price = details[1]
+        price = extract_price(details[1])
 
-        Food(name, tname, detail, price, food_category[id], sub_cat)
+        category_id = make_abbreviation(category_name)
+        sub_cat_id = make_abbreviation(sub_cat)
+
+        Food(name, tname, detail, price, category_id, sub_cat_id)
 
 
 soup = getContent("https://www.tgifridays.co.jp/drinks/")
 
 # locating the drink category menu
-drink_cat = soup.find("div", {"id":"ultimates"})
-liList = drink_cat.find_all('li', class_="menu-item")
+drink_menu = soup.find("div", {"id":"ultimates"})
+liList = drink_menu.find_all('li', class_="menu-item")
 
-drinks = dict()
+drink_category = dict()
 
 # getting list of drink categories and translations
 for li in liList:
-    category_name = li.a.contents[0]+'/'+li.a.span.text
+    category_name = li.a.contents[0] # +'/'+li.a.span.text
 
-    drinks[name] = {}
+    drink_category[category_name] = []
 
     pList = li.div.find_all("p")
 
-    sub_cat = ''
+    sub_cat = None
     for p in pList:
         if p.has_attr('class'):
             sub_cat = p.text
+            drink_category[category_name].append(sub_cat)
             continue
         
         drink = getStrippedString(p)
@@ -85,10 +122,13 @@ for li in liList:
 
         drink = drink.split("/")
         name = drink[0]
-        price = drink[1]
+        price = extract_price(drink[1])
 
-        Drink(name, tname, '', price, category_name, sub_cat)
+        category_id = make_abbreviation(category_name)
+        sub_cat_id = make_abbreviation(sub_cat)
 
-for food in Food.all:
-    print(food.__dict__)
+        Drink(name, tname, '', price, category_id, sub_cat_id)
+
+
+
 exit()
