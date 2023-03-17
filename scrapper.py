50e8f8506c8cba1a1a3459b416ebdf06eb8e2445
Vladimer Redula <vdredula@gmail.com>
2023-03-17 13:09:07 +0900

    adding code to insert values into database

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 from item import Food, Drink
+from db import DB_connector
 
 def getContent(url):
     response = requests.get(url)
@@ -130,5 +131,85 @@ for li in liList:
         Drink(name, tname, '', price, category_id, sub_cat_id)
 
 
+
+db = DB_connector('postgres','postgres','password','localhost','5432')
+
+db.connect()
+
+with open('tables.sql', 'r') as file:
+    sql = file.read()
+
+db.execute_query(sql)
+
+# insert type
+db.execute_query("TRUNCATE public.sub_category CASCADE")
+db.execute_query("TRUNCATE public.category CASCADE")
+db.execute_query("TRUNCATE public.items CASCADE")
+
+categories = {**food_category, **drink_category}
+
+# insert categories
+for category, sub_categories in categories.items():
+    cat_abbr = make_abbreviation(category)
+
+    if category in food_category.keys():
+        item_type = 'F'
+    else:
+        item_type = 'D'
+
+    sql = "INSERT INTO public.category (id, name, classificationid) VALUES (%s, %s, %s);"
+    
+    db.execute_query(sql, [cat_abbr, category, item_type])
+
+    if sub_categories != []:
+        for sub_cat in sub_categories:
+            subcat_abbr = make_abbreviation(sub_cat)
+            sql = "INSERT INTO public.sub_category (id, name, categoryid) VALUES (%s, %s, %s);"
+
+            db.execute_query(sql, [subcat_abbr, sub_cat, cat_abbr])
+
+
+for item in Food.all:
+    food = item.__dict__
+
+    sql = "INSERT INTO public.items (name, tname, description, price, categoryid, subcatid, classificationid) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+
+    values = [
+        food['name'],
+        food['tname'],
+        food['description'],
+        food['price'],
+        food['category'],
+        food['subcategory'],
+        'F'
+    ]
+
+    print(values)
+
+    db.execute_query(sql, values)
+
+for item in Drink.all:
+    drink = item.__dict__
+
+    sql = "INSERT INTO public.items (name, tname, description, price, categoryid, subcatid, classificationid) VALUES (%s, %s, %s, %s, %s, %s, %s);"
+
+    values = [
+        drink['name'],
+        drink['tname'],
+        drink['description'],
+        drink['price'],
+        drink['category'],
+        drink['subcategory'],
+        'D'
+    ]
+
+    print(values)
+
+    db.execute_query(sql, values)
+
+# insert items
+
+
+db.close_connection()
 
 exit()
