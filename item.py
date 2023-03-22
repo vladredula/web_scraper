60f59db47ccd54484f7726d44f2221a2bae17bbc
Vladimer Redula <vdredula@gmail.com>
2023-03-23 01:10:10 +0900

    removed class: Drink, Food

--- item.py
+++ item.py
@@ -1,44 +1,16 @@
 
 class Item:
     all = []
-    def __init__(self, name, tname, description, price):
+    def __init__(self, name, tname, description, price, category, subcategory, img_url):
         self.name = name
         self.tname = tname
         self.description = description
         self.price = price
+        self.category = category
+        self.subcategory = subcategory
+        self.img_url = img_url
 
         Item.all.append(self)
 
     #def __repr__(self):
     #    return f'{self.__class__.__name__},{self.name}/{self.tname},{self.desc},{self.price}'
-
-
-class Food(Item):
-    all = []
-    def __init__(self, name, tname, description, price, category, img_url, subcategory = None):
-        # Call to super function to have access to all attributes / methods
-        super().__init__(
-            name, tname, description, price
-        )
-
-        # Assign to self object
-        self.category = category
-        self.subcategory = subcategory
-        self.img_url = img_url
-
-        Food.all.append(self)
-
-
-class Drink(Item):
-    all = []
-    def __init__(self, name, tname, description, price, category, subcategory = None):
-        # Call to super function to have access to all attributes / methods
-        super().__init__(
-            name, tname, description, price
-        )
-
-        # Assign to self object
-        self.category = category
-        self.subcategory = subcategory
-
-        Drink.all.append(self)
