bf1c65344cc1b4edfdd2a3b01c616399dcceeb58
Vladimer Redula <vdredula@gmail.com>
2023-03-25 00:25:44 +0900

    fixes and modification on scape funtion

--- scrapper.py
+++ scrapper.py
@@ -2,17 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 import re
 from item import Item
+from category import Category
+import pprint
 
 def getContent(url):
     response = requests.get(url)
     content = response.content
     content = BeautifulSoup(content, "html.parser")
+    
     return content
 
 
 def getStrippedString(str):
     str = str.stripped_strings
+    
     return list(str)
+
 
 def extract_price(str):
     str = ''.join(c for c in str if c.isalnum())
@@ -25,13 +30,15 @@ def extract_price(str):
 
     return price[1]
 
+
 def make_abbreviation(str):
-    if str == None:
-        return None
-    
+    if str == '':
+        return str
+        
+    str = str.strip()
     abbreviation = str[0]
 
-    vowels = 'AEIOU'
+    vowels = 'aeiouAEIOU'
     new_str = ''
     for s in str[1:]:
         if s not in vowels and s.isalnum():
@@ -41,95 +48,99 @@ def make_abbreviation(str):
         if i < len(new_str):
             abbreviation += new_str[i]
 
-    return abbreviation.upper()
+    return abbreviation.lower()
+    
 
+def scrape():
+    soup = getContent("https://www.tgifridays.co.jp/foods/")
 
-soup = getContent("https://www.tgifridays.co.jp/foods/")
+    # locating the food category menu
+    cat_menu = soup.find("div", {"id":"controller"})
+    aList = cat_menu.findAll('a')
+    
+    food_category = dict()
 
-# locating the food category menu
-cat_menu = soup.find("div", {"id":"controller"})
-aList = cat_menu.findAll('a')
+    # getting list of food categories
+    for a in aList:
+        category_id = a['rel'][0].replace('#','')
+        category_name = a.text.strip()
+        food_category[category_id] = category_name
 
-food_category = dict()
+    for category_id, category_name in food_category.items():
+        cat_img_url = soup.find("a", {"class":"nolink","rel":"#"+category_id})
 
-# getting list of food categories
-for a in aList:
-    id = a['rel'][0].replace('#','')
-    category_name = a.text
-    food_category[category_name] = id
-
-
-for category_name, id in food_category.items():
-    li = soup.find("div", {"id":id})
-    liList = li.findAll("li", class_="menu-item")
-
-    # getting the list of food under each category
-    food_category[category_name] = []
-    sub_cat = ''
-    for li in liList:
-        if len(li['class']) > 1:
-            sub_cat = li.text
-            food_category[category_name].append(sub_cat)
-            continue
-
-        img = li.img['src']
-
-        food = getStrippedString(li.a)
-        name = ' '.join(food[:-1])
-        tname = food[-1]
-
-        details = getStrippedString(li.div.p)
-        details = ' '.join(details)
-
-        details = details.rsplit('！', 1)
-
-        if len(details) == 1:
-            details = details[0].rsplit('。', 1)
-
-        detail = details[0].replace(tname, "")
-        price = extract_price(details[1])
-
-        Item(name, tname, detail, price, category_name, sub_cat, img)
-
-
-soup = getContent("https://www.tgifridays.co.jp/drinks/")
-
-# locating the drink category menu
-drink_menu = soup.find("div", {"id":"ultimates"})
-liList = drink_menu.find_all('li', class_="menu-item")
-
-drink_category = dict()
-
-# getting list of drink categories and translations
-for li in liList:
-    category_name = li.a.contents[0].text # +'/'+li.a.span.text
-
-    drink_category[category_name] = []
-
-    pList = li.div.find_all("p")
-
-    sub_cat = ''
-    for p in pList:
-        if p.has_attr('class'):
-            sub_cat = p.text
-            drink_category[category_name].append(sub_cat)
-            continue
+        cat_img_url = cat_img_url.img['src']
+        category_name = category_name.lower()
+        category_abbr = make_abbreviation(category_name)
         
-        drink = getStrippedString(p)
-        tname = drink[-1]
-        drink = ' '.join(drink[:-1])
+        Category(category_name, category_abbr, 'F', cat_img_url)
 
-        drink = drink.split("/")
-        name = drink[0]
-        price = extract_price(drink[1])
+        li = soup.find("div", {"id":category_id})
+        liList = li.findAll("li", class_="menu-item")
+    
+        # getting the list of food under each category
+        sub_cat = ""
+        for li in liList:
+            if len(li['class']) > 1:
+                sub_cat = li.text
+                continue
+    
+            img = li.img['src']
+    
+            food = getStrippedString(li.a)
+            name = ' '.join(food[:-1])
+            tname = food[-1]
+    
+            details = getStrippedString(li.div.p)
+            details = ' '.join(details)
+    
+            details = details.rsplit('！', 1)
+    
+            if len(details) == 1:
+                details = details[0].rsplit('。', 1)
+    
+            detail = details[0].replace(tname, "")
+            price = extract_price(details[1])
+    
+            Item(name, tname, detail, price, category_abbr, sub_cat, img, 'F')
+    
 
-        Item(name, tname, '', price, category_name, sub_cat, '')
+    soup = getContent("https://www.tgifridays.co.jp/drinks/")
+    
+    # locating the drink category menu
+    drink_menu = soup.find("div", {"id":"ultimates"})
+    liList = drink_menu.find_all('li', class_="menu-item")
+    
+    # getting list of drink categories and translations
+    for li in liList:
+        category_name = li.a.contents[0].text.lower().strip()
+        category_abbr = make_abbreviation(category_name)
+        
+        Category(category_name, category_abbr, 'D')
+    
+        pList = li.div.find_all("p")
+    
+        sub_cat = ""
+        for p in pList:
+            if p.has_attr('class'):
+                sub_cat = p.text
+                continue
+            
+            drink = getStrippedString(p)
+            tname = drink[-1]
+            drink = ' '.join(drink[:-1])
+    
+            drink = drink.split("/")
+            name = drink[0]
+            price = extract_price(drink[1])
+    
+            Item(name, tname, '', price, category_abbr, sub_cat, '', 'D')
 
+    for item in Category.all:
+        print(item.__dict__)
 
-for id, item in enumerate(Item.all):
-    food = item.__dict__
-    food['id'] = id+1
-    if id == 56:
-        print(food)
-        for key, value in food.items():
-            print(key, type(value))
+    for item in Item.all:
+        print(item.__dict__)
+    exit()
+    
+scrape()
