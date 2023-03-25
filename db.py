bf91cf8b451777c0d9fd272fc1151cc6efb1a97a
Vladimer Redula <vdredula@gmail.com>
2023-03-25 18:13:21 +0900

    added exception handling to dynamodb class

--- db.py
+++ db.py
@@ -1,63 +1,98 @@
 import boto3
+from botocore.exceptions import ClientError
+import logging
+
+logger = logging.getLogger(__name__)
 
 class DynamoDB:
     def __init__(self):
-        self.client = boto3.client('dynamodb')
-        self.resource = boto3.resource('dynamodb')
+        self.resource = boto3.resource('dynamodb', region_name='ap-northeast-1')
+        self.table = None
     
     # Create a new table with the specified name and primary key
     def create_table(self, table_name, primary_key):
-        response = self.client.create_table(
-            TableName = table_name,
-            KeySchema = [
-                {
-                    'AttributeName': primary_key,
-                    'KeyType': 'HASH'
+        try:
+            self.table = self.resource.create_table(
+                TableName = table_name,
+                KeySchema = [
+                    {
+                        'AttributeName': primary_key,
+                        'KeyType': 'HASH'
+                    }
+                ],
+                AttributeDefinitions = [
+                    {
+                        'AttributeName': primary_key,
+                        'AttributeType': 'S'
+                    }
+                ],
+                ProvisionedThroughput = {
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
                 }
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': primary_key,
-                    'AttributeType': 'S'
-                }
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 5,
-                'WriteCapacityUnits': 5
-            }
-        )
-        return response
+            )
+            self.table.wait_until_exists()
+            
+        except ClientError as e:
+            logger.error(
+                "Couldn't create table %s. Here's why: %s: %s", table_name,
+                e.response['Error']['Code'], e.response['Error']['Message'])
+            raise
+        else:
+            return self.table
 
     # Add a new item to a DynamoDB table
-    def put_item(self, table_name, item):
-        table = self.get_table_resource(table_name)
-        response = table.put_item(Item = item)
-        return response
+    def put_item(self, item):
+        try:
+            self.table.put_item(Item = item)
+        except ClientError as e:
+            logger.error(
+                "Couldn't add item to table %s. Here's why: %s: %s",
+                self.table.name,
+                e.response['Error']['Code'], e.response['Error']['Message'])
+            raise
         
     # Check if table exists in DynamoDB
     def table_exists(self, table_name):
-        response = self.client.list_tables()
-        if table_name in response['TableNames']:
-            return True
+        try:
+            table = self.resource.Table(table_name)
+            table.load()
+            exists = True
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                exists = False
+            else:
+                logger.error(
+                    "Couldn't check for existence of %s. Here's why: %s: %s",
+                    self.table.name,
+                    e.response['Error']['Code'], e.response['Error']['Message'])
+                raise
         else:
-            return False
-
+            self.table = table
+        return exists
  
-    def truncate_table(self, table_name):
-        table = self.get_table_resource(table_name)
+    def truncate_table(self):
+        # get all items in the table first
+        try:
+            scan = self.table.scan(
+                ProjectionExpression='#k',
+                ExpressionAttributeNames = {'#k': 'id'}
+            )
+        except ClientError as e:
+            logger.error(
+                "Couldn't scan table %s. Here's why: %s: %s", 
+                self.table.name,
+                e.response['Error']['Code'], e.response['Error']['Message'])
+            raise
         
-        scan = table.scan(
-            ProjectionExpression='#k',
-            ExpressionAttributeNames={
-                '#k': 'id'
-            }
-        )
-        
-        with table.batch_writer() as batch:
-            for each in scan['Items']:
-                batch.delete_item(Key=each)
-                
-    def get_table_resource(self, table_name):
-        table = self.resource.Table(table_name)
-        
-        return table
+        # Delete items in batches
+        with self.table.batch_writer() as batch:
+            for item in scan['Items']:
+                try:
+                    batch.delete_item(Key = item)
+                except ClientError as e:
+                    logger.error(
+                        "Couldn't delete item in table %s. Here's why: %s: %s", 
+                        self.table.name,
+                        e.response['Error']['Code'], 
+                        e.response['Error']['Message'])
